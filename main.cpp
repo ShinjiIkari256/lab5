1840cf77f19a0b77ef58d42450b7d608eec099df
ShinjiIkari256 <hayato.gokudera1990@ya.ru>
2018-05-10 22:41:20 +0300

    2.5.1 main.cpp in clone changed: added -

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main()
 	int a, b;
 	cout << "Enter A and B: ";
 	cin >> a >> b;
-	cout << "A + B = " << a + b << '\n';
+	cout << "A + B = " << a + b << '\n'
+		 << "A - B = " << a - b << '\n';
 	return 0;
 }
